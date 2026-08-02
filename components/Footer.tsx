d29445055fd95aa9978f75e927f4de4efffd6c2e
Cloud Hayes <Cloud@gproconnect.com>
2026-08-02 14:09:29 -0500

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,3 @@
-import MembersFooterButton from './MembersFooterButton';
-
 export default function Footer() {
   return (
     <footer className="mt-auto bg-footer px-5 py-8 text-white/75">
@@ -23,7 +21,6 @@ export default function Footer() {
           Facebook Group
         </a>
       </div>
-      <MembersFooterButton />
     </footer>
   );
 }
